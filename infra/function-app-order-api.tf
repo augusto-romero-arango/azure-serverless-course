cd235db86757a07ecc8b6e3a91b4815180dc64e6
augusto-romero-arango <a.romeroa23@uniandes.edu.co>
2026-02-03 12:16:25 -0500

    Enhance application insights and logging configuration for order API function

--- infra/function-app-order-api.tf
+++ infra/function-app-order-api.tf
@@ -34,13 +34,60 @@ resource "azurerm_linux_function_app" "order-api-func" {
   }
   
   app_settings = { 
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "AzureWebJobsStorage" = azurerm_storage_account.main.primary_connection_string 
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app_insights.instrumentation_key
+    FUNCTIONS_WORKER_RUNTIME = "python"
+    AzureWebJobsStorage = azurerm_storage_account.main.primary_connection_string 
+    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.app_insights.instrumentation_key
+
+    APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.app_insights.connection_string
+    APPLICATIONINSIGHTSAGENT_EXTENSION_VERSION = "~3"
   }
 
   tags = { 
         "func" =  "${var.project_name}-api-${var.environment}"  
         }
 }
-  
+
+data "azurerm_monitor_diagnostic_categories" "func" {
+  resource_id = azurerm_linux_function_app.order-api-func.id
+}
+
+locals {
+  desired_logs = toset([
+    "FunctionAppLogs",
+    "AppServiceConsoleLogs",
+    "AppServiceAppLogs",
+    "AppServiceHTTPLogs",
+    "AppServicePlatformLogs"
+  ])
+
+  available_logs = toset(data.azurerm_monitor_diagnostic_categories.func.azurerm_monitor_diagnostic_categories[*].name)
+  enabled_logs = setintersection(local.desired_logs, local.available_logs)
+
+  desired_metrics = toset([
+    "AllMetrics"
+  ])
+  available_metrics = toset(data.azurerm_monitor_diagnostic_categories.func.azurerm_monitor_diagnostic_categories[*].name)
+  enabled_metrics = setintersection(local.desired_metrics, local.available_metrics)
+
+
+}
+
+resource "azurerm_monitor_diagnostic_setting" "func_to_law" {
+  name = "${var.project_name}-api-${var.environment}-func-diag"
+  target_resource_id = azurerm_linux_function_app.order-api-func.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+
+  dynamic "enabled_log" {
+    for_each = local.enabled_logs
+    content {
+      category = enabled_log.value
+    }
+  }
+  dynamic "enabled_metric" {
+    for_each = local.enabled_metrics
+    content {
+      category = enabled_metric.value
+    }
+  }
+
+}
