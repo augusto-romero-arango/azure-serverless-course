19c872c50a5c49ff64a0ff09ed941afdada9b42a
augusto-romero-arango <a.romeroa23@uniandes.edu.co>
2026-02-04 18:36:37 -0500

    Add Azure Storage account and container resources

---
+++ infra/order-storage.tf
@@ -0,0 +1,13 @@
+resource "azurerm_storage_account" "file_storage" {
+  name = "order${var.environment}st"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  account_tier        = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "receipts" {
+  name                  = "receipts"
+  storage_account_id = azurerm_storage_account.file_storage.id
+  container_access_type = "private"
+}
