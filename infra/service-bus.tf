4e020b75b6ec710a93c452d2e38312413d802c75
augusto-romero-arango <a.romeroa23@uniandes.edu.co>
2026-02-04 12:26:03 -0500

    Add Service Bus namespace, queue, and authorization rule resources

---
+++ infra/service-bus.tf
@@ -0,0 +1,19 @@
+resource "azurerm_servicebus_namespace" "namespace" {
+  name= "${var.project_name}-${var.environment}-sb"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku = "Standard"
+}
+
+resource "azurerm_servicebus_queue" "order_queue" {
+  name = "notifications-queue"
+  namespace_id = azurerm_servicebus_namespace.namespace.id
+  max_delivery_count = 5
+}
+
+resource "azurerm_servicebus_namespace_authorization_rule" "auth_rule" {
+  name = "send_policy"
+  namespace_id = azurerm_servicebus_namespace.namespace.id
+
+  send = true
+}
