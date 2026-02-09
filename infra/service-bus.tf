8df47fe44d5950ab7651be798926a8f2e5221320
augusto-romero-arango <a.romeroa23@uniandes.edu.co>
2026-02-09 17:20:56 -0500

    Fix metric name for dead letter queue alert in service-bus.tf

--- infra/service-bus.tf
+++ infra/service-bus.tf
@@ -51,7 +51,7 @@ resource "azurerm_monitor_metric_alert" "alert_dlq" {
 
   criteria {
     metric_namespace = "Microsoft.ServiceBus/namespaces"
-    metric_name = "dlq_messages_count"
+    metric_name = "DeadletteredMessages"
     aggregation = "Total"
     operator = "GreaterThan"
     threshold = 0
