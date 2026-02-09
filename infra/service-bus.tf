4898e611cfd2942b1dd1fbc115a483da0d0d1bd9
augusto-romero-arango <a.romeroa23@uniandes.edu.co>
2026-02-09 17:10:06 -0500

    Add monitoring for dead letter queue with action group and metric alert

--- infra/service-bus.tf
+++ infra/service-bus.tf
@@ -25,3 +25,41 @@ resource "azurerm_servicebus_namespace_authorization_rule" "auth_rule" {
   send = true
 }
 
+resource "azurerm_monitor_action_group" "ops" {
+  name = "${var.project_name}-ops-${var.environment}-ag"
+  resource_group_name = azurerm_resource_group.main.name
+  short_name = "ops"
+
+  email_receiver {
+    name = "email_dlq_alerts"
+    email_address = "augromara@gmail.com"
+    
+    }
+}
+
+resource "azurerm_monitor_metric_alert" "alert_dlq" {
+  name = "${var.project_name}-${var.environment}-dlq-ag"
+  resource_group_name = azurerm_resource_group.main.name
+  scopes = [azurerm_servicebus_namespace.namespace.id]
+  description = "Alert for dead lettered messages in Service Bus Queue"
+  severity = 2
+  frequency = "PT1M"
+  window_size = "PT5M"
+  enabled = true
+  target_resource_type = "Microsoft.ServiceBus/namespaces"
+  target_resource_location = azurerm_resource_group.main.location
+
+  criteria {
+    metric_namespace = "Microsoft.ServiceBus/namespaces"
+    metric_name = "dlq_messages_count"
+    aggregation = "Total"
+    operator = "GreaterThan"
+    threshold = 0
+  
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.ops.id
+  } 
+}
+
